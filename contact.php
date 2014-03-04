134fb9dcae6d8dcfba01b35781412a11e14f43db
iryanclarke <clarke_93@hotmail.com>
2014-03-03 20:04:55 -0500

    Oops, deleted files

---
+++ contact.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Contact</title>
+
+<!-- Stylesheets-->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" media="all" href="css/banner.css" />
+	<link rel="stylesheet" media="all" href="css/pages.css" />
+    <link rel="shortcut icon" href="img/favicon.ico">
+<!-- jQuery and js -->
+	<script src="js/modernizr.custom.37797.js"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+    <script src="js/jquery.queryloader2.js"></script>
+</head>
+
+<body>
+
+    <?php
+        include 'banner.php';
+    ?> 
+   
+   
+   <div id="content">
+            <div style="padding-top:70px">
+                  <h2 style="float:right;padding-right:150px;"><span style="background-color:#006ba4;padding:10px">Contact me</span></h2>
+                  <p>&nbsp;</p>
+                  <div style="clear:both"></div>
+            </div>  
+   </div> 
+   
+    <div class="section1">
+       <div id="content" style="display:block;overflow:auto">
+       		<h3>My Life</h3>
+            <ul class='icoList'>
+            	<a href="#"><li class="email">ian@iryanclarke.com</li></a>
+                <li class="linkedin">Asdsasdsa</li>
+                <li class="twitter">qwerqwe</li>
+                <li class="github">qrer</li>
+                <li class="stack">Asdsarrrsdsa</li>
+                <li class="email">Asdsagggsdsa</li>
+                <li class="email">sdgfdgd</li>
+            </ul>
+        </div> 
+    </div>    
+    
+    <div class="section2">   
+        <div id="content">
+        	<h3>What I love</h3>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+            <p>Content is coming soon! :)</p>
+        </div>
+    </div>    
+    <script src="js/gAnalytics.js"></script>
+         
+    <div class="footer">
+    	<p1>Design and Development by Ian Clarke</p1>
+    </div>
+</body>
+
+</html>
---
+++ portfolio.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Portfolio</title>
+
+<!-- Stylesheets-->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,500,500italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" media="all" href="css/banner.css" />
+	<link rel="stylesheet" media="all" href="css/pages.css" />
+    <link rel="shortcut icon" href="img/favicon.ico">
+<!-- jQuery and js -->
+	<script src="js/modernizr.custom.37797.js"></script> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+    <script src="js/jquery.queryloader2.js"></script>
+    <script src="js/iGrid.js"></script>
+</head>
+
+<body>
+
+    <?php
+        include 'banner.php';
+    ?> 
+   
+   <div id="content">
+            <div style="padding-top:70px">
+                  <h2 style="float:right;padding-right:150px;"><span style="background-color:#006ba4;padding:10px">Portfolio</span></h2>
+                  <p>&nbsp;</p>
+                  <div style="clear:both"></div>
+            </div>   
+   </div>         
+   
+    
+    <div class="section1">   
+        <div id="content">
+        	<main>
+            <h1>Photoset (advanced)</h1>
+                <hr />
+                <div class="photoset">
+                    <div class="photoset-row">
+                        <figure class="photoset-item">
+                            <a href="http://500px.com/photo/4772670"><img src="http://i.imgur.com/uh5YLj5.jpg" alt="ARoS Aarhus Kunstmuseum" title="ARoS Aarhus Kunstmuseum" /></a>
+                            <figcaption>ARoS Aarhus Kunstmuseum. Photo &copy; Terry Mun</figcaption>
+                        </figure>
+                    </div>
+                    <div class="photoset-row">
+                        <figure class="photoset-item">
+                            <a href="http://500px.com/photo/4820790"><img src="http://i.imgur.com/24hrPQn.jpg" alt="A lady walks briskly on a train platform in Bern, Switzerland." title="A lady walks briskly on a train platform in Bern, Switzerland." /></a>
+                            <figcaption>A lady walks briskly on a train platform in Bern, Switzerland. Photo &copy; Terry Mun</figcaption>
+                        </figure>
+                        <figure class="photoset-item">
+                            <a href="http://500px.com/photo/20245695"><img src="http://i.imgur.com/BXo4qAz.jpg" alt="Inside Rundetårn, Copenhagen." title="Inside Rundetårn, Copenhagen." /></a>
+                            <figcaption>Inside Rundetårn, Copenhagen. Photo &copy; Terry Mun</figcaption>
+                        </figure>
+                    </div>
+                    <div class="photoset-row">
+                        <figure class="photoset-item">
+                            <a href="http://500px.com/photo/39822810"><img src="http://i.imgur.com/Vo922iy.jpg" alt="By Twilight." title="By Twilight." /></a>
+                            <figcaption>By Twilight. Photo &copy; Terry Mun</figcaption>
+                        </figure>
+                        <figure class="photoset-item">
+                            <a href="http://500px.com/photo/35823988"><img src="http://i.imgur.com/80WaVuY.jpg" alt="God fest på Mejlgade." title="God fest på Mejlgade." /></a>
+                            <figcaption>God fest på Mejlgade. Photo &copy; Terry Mun</figcaption>
+                        </figure>
+                        <figure class="photoset-item">
+                            <a href="http://500px.com/photo/13998781"><img src="http://i.imgur.com/uDPcnM3.jpg" alt="Sit Back" title="Sit Back" /></a>
+                            <figcaption>Sit Back. Photo &copy; Terry Mun</figcaption>
+                        </figure>
+                    </div>
+                <div class="photoset-row">
+                  <figure class="photoset-item">
+                    <a href="http://500px.com/photo/5889975"><img src="http://i.imgur.com/esWWGbF.jpg" alt="Basilique du Sacré-Cœur." title="Basilique du Sacré-Cœur." /></a>
+                    <figcaption>Basilique du Sacré-Cœur. Photo &copy; Terry Mun</figcaption>
+                  </figure>
+                  <figure class="photoset-item">
+                    <a href="http://500px.com/photo/4772520"><img src="http://i.imgur.com/ZCogT10.jpg" alt="Cyclist in Berlin" title="Cyclist in Berlin" /></a>
+                    <figcaption>Cyclist in Berlin. Photo &copy; Terry Mun</figcaption>
+                  </figure>
+                </div>
+                <div class="photoset-row">
+                  <figure class="photoset-item">
+                    <a href="http://500px.com/photo/5489150"><img src="http://i.imgur.com/UvGHJjo.jpg" alt="At the annual sailing race in Aarhus University." title="At the annual sailing race in Aarhus University." /></a>
+                    <figcaption>At the annual sailing race in Aarhus University. Photo &copy; Terry Mun</figcaption>
+                  </figure>
+                  <figure class="photoset-item">
+                    <a href="http://500px.com/photo/5889990"><img src="http://i.imgur.com/rWuQotb.jpg" alt="Dark corridors." title="Dark corridors." /></a>
+                    <figcaption>Dark corridors. Photo &copy; Terry Mun</figcaption>
+                  </figure>
+                </div>
+                </div>
+            </main>
+        </div>
+    </div>   
+    <script src="js/gAnalytics.js"></script> 
+         
+    <div class="footer">
+    	<p1>Design and Development by Ian Clarke</p1>
+    </div>
+</body>
+
+</html>
