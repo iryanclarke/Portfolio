9e229a0d9fbb419e0bde59e7a28f653e9d0f19ae
iryanclarke <clarke_93@hotmail.com>
2014-05-22 17:01:53 -0400

    About me page updates

--- about.php
+++ about.php
@@ -37,9 +37,7 @@
                 <h3 class="background"><img src="img/icons/journal.png" align="middle">Background</h3>
                 <p>My interest and love for Web and Graphic design began way back in grade 9, when I took a tech class and began hashing out websites using good 'ol DreamWeaver's <b>design</b> view. <i>(I now shudder)</i> </p>
                 <p>I now currently attend Carleton University for the Bachelor of Information Technology, in the stream of Interactive Multimedia and Design. It is here where my love for Web Development and Illustration came to fruition. </p>
-                <p>Content is coming soon! :)</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
+                <p>I have worked for the Canadian Medical Protective Association where I was a Web Developer and QA analyst, and Kivuto Solutions where I was a front-end developer. I enjoy being a part of internal teams, such as the Eats and Entertainment Committee at Kivuto Solutions, where we would plan montly company events. </p>
                 <p>&nbsp;</p>
                 <p>&nbsp;</p>
             </div> 
@@ -49,10 +47,8 @@
             <div class="content">
                 <h3><img src="img/icons/heart.png" align="middle">Interests</h3>
                 <p></p>
-                <p>Nulla viverra, erat sit amet pellentesque facilisis, urna ligula consectetur eros, at laoreet purus sem non dolor. Proin suscipit est aliquet velit pulvinar, vel vulputate erat mollis. Duis nec lectus ultricies, volutpat enim dictum, ullamcorper tortor. Fusce vel est non ipsum ultricies cursus. Sed id risus non lacus dapibus tempor a sed odio. Donec non rhoncus tortor. Donec pharetra ut dolor eu accumsan. Nunc mi lacus, vulputate vitae placerat id, aliquam ut mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Aenean tortor erat, pharetra in lorem sed, ultrices pellentesque est. Vivamus faucibus, erat eleifend dapibus commodo, sem odio pharetra augue, at tempor quam purus quis leo. Aliquam sed est vitae leo cursus mattis. Proin vel nulla sit amet augue posuere rhoncus.</p>
-                <p>Content is coming soon! :)</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
+                <p>I spend most of my spare time reading web dev blogs such as Smashing Magazine, Abduzeedo, Net tuts, and David Walsh blog, and hacking around on projects I find cool! I absolutely have a soft spot for flat design, and I sometimes mess around in illustrator with some flat designs and complementary colour schemes.</p>
+                <p>When I'm not nerding out on the computer, you can find me tinkering with my R/C helicopters, or playing Starcraft II and Age of Mythology with my friends. I also attend Web Related meetups in Ottawa, such as Ottawa Ruby, AngularJS and Ottawa JavaScript!</p>
                 <p>&nbsp;</p>
                 <p>&nbsp;</p>
             </div>
@@ -61,11 +57,24 @@
         <div class="section3">   
             <div class="content">
             	<h3><img src="img/icons/heart.png" align="middle">Skills and Tools</h3>
-                <p></p>
-                <p>Nulla viverra, erat sit amet pellentesque facilisis, urna ligula consectetur eros, at laoreet purus sem non dolor. Proin suscipit est aliquet velit pulvinar, vel vulputate erat mollis. Duis nec lectus ultricies, volutpat enim dictum, ullamcorper tortor. Fusce vel est non ipsum ultricies cursus. Sed id risus non lacus dapibus tempor a sed odio. Donec non rhoncus tortor. Donec pharetra ut dolor eu accumsan. Nunc mi lacus, vulputate vitae placerat id, aliquam ut mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Aenean tortor erat, pharetra in lorem sed, ultrices pellentesque est. Vivamus faucibus, erat eleifend dapibus commodo, sem odio pharetra augue, at tempor quam purus quis leo. Aliquam sed est vitae leo cursus mattis. Proin vel nulla sit amet augue posuere rhoncus.</p>
-                <p>Content is coming soon! :)</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
+                <p>I have acquired a long list of programs and languages I am familiar with over the years, so i'll break it down into ones that I use very often!</p>
+                <ul>
+                    <li>Sublime text 2: For ALL my coding needs!</li>
+                    <li>Koala: A preprocessor for stuff like SASS, LESS, and CoffeeScript</li>
+                    <li>Chrome: The browser of choice!</li>
+                    <li>SASS: Only about 300x better than plain old CSS!</li>
+                    <li>Git: Version control, the best. 'Nuff said.</li>
+                    <li>PHP: For excellent backend!</li>
+                    <li>jQuery/JavaScript: For excellent backend!</li>
+                    <li>Foundation 5: One of the best frameworks out there!</li>
+                </ul>
+                <p>Here is a list of more programs and languages I know!</p>
+                <ul>
+                    <li>Adobe Creative Suite (Fireworks, Flash, Dreamweaver, Premier Pro)</li>
+                    <li>ColdFusion, MySQL, AJAX, JSON</li>
+                    <li>C, C++, Actionscript 3.0</li>
+                    <li>Microsoft Visual Studio, Autodesk Maya, Eclipse</li>
+                </ul>
                 <p>&nbsp;</p>
                 <p>&nbsp;</p>
             </div>
