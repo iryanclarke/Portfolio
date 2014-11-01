654fae7cc4b796b3e17ae08d534ab8f4b130ea34
iryanclarke <clarke_93@hotmail.com>
2014-11-01 16:58:37 -0400

    Final grunt files and BrowserSync working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,46 @@
+
+'use strict';
+ 
 module.exports = function (grunt) {
+ 
+  // Project configuration.
   grunt.initConfig({
     // Watch task config
     watch: {
       sass: {
         files: "css/scss/*.scss",
-        tasks: ['sass']
+        tasks: "sass:dev"
       }
     },
-    // SASS task config
     sass: {
-        dev: {
-            files: {
-                // destination      // source file
-                "css/css/mobile.css" : "css/scss/mobile.scss"
-                "css/css/desktop.css" : "css/scss/desktop.scss"
-            }
+      dev: {
+        files: {
+          // destination      // source file
+          "css/css/mobile.css" : "css/scss/mobile.scss",
+          "css/css/desktop.css" : "css/scss/desktop.scss"
         }
+      }
     },
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            "css/css/*.css",
+            "*.html"
+          ]
+        },
+        options: {
+          watchTask: true,
+          proxy: "localhost"
+        }
+      }
+    }
   });
-
-  grunt.loadNpmTasks('grunt-contrib-sass');
+ 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-browser-sync');
+ 
+  // Launch BrowserSync + watch task
+  grunt.registerTask('default', ['browserSync', 'watch']);
 };
--- index-b.php
+++ index-b.php
@@ -9,8 +9,8 @@
 	<meta name="description" content="Ian's Portfolio" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <!-- Stylesheets-->
-  <link rel="stylesheet" href="css/mobile.css">
-  <link rel="stylesheet" href="css/desktop.css" media="(min-width:50em)">
+  <link rel="stylesheet" href="css/css/mobile.css">
+  <link rel="stylesheet" href="css/css/desktop.css" media="(min-width:50em)">
   <link rel="stylesheet" href="css/fonts/font-awesome/css/font-awesome.min.css">
   <!-- jQuery and js -->
   <script src="js/jquery-1.9.1.min.js"></script>
