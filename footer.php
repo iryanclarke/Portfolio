cac71d18b3e52102bd73086959b6927f1b8ae799
iryanclarke <clarke_93@hotmail.com>
2014-12-26 22:51:48 -0500

    Attempting to make footer sticky

--- footer.php
+++ footer.php
@@ -1,28 +1,28 @@
+</div>
 
-      <div class="push"></div>
-    
 
-<footer class="footer-2">
-      <div class="footer-logo">
-        <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="">
-      </div>
-        <ul>
-          <li><a href="about.php">Design and Development by Ian Ryan Clarke</a></li>
-        </ul>
-
-        <div class="footer-secondary-links">
-          <ul>
-            <li><a href="javascript:void(0)">Privacy Policy, Copyright &copy; 2014 Ian Ryan Clarke </a></li>
-          </ul>
-
-          <ul class="footer-social">
-            <li><a href="https://twitter.com/tridotdesigns">
-              <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/facebook-logo-circle.png" alt="">
-              </a></li>
-            <li><a href="https://twitter.com/tridotdesigns">
-              <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="">
-            </a></li>
-          </ul>
+<div class="push">
+  <footer class="footer-2">
+        <div class="footer-logo">
+          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png" alt="">
         </div>
-</footer>
+          <ul>
+            <li><a href="about.php">Design and Development by Ian Ryan Clarke</a></li>
+          </ul>
+
+          <div class="footer-secondary-links">
+            <ul>
+              <li><a href="javascript:void(0)">Privacy Policy, Copyright &copy; 2014 Ian Ryan Clarke </a></li>
+            </ul>
+
+            <ul class="footer-social">
+              <li><a href="https://twitter.com/tridotdesigns">
+                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/facebook-logo-circle.png" alt="">
+                </a></li>
+              <li><a href="https://twitter.com/tridotdesigns">
+                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="">
+              </a></li>
+            </ul>
+          </div>
+  </footer>
 </div>
