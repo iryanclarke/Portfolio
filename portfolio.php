e4599bb8b0de269e1554eed5288a8d3831dd242d
iryanclarke <clarke_93@hotmail.com>
2015-01-15 12:24:52 -0500

    Portfolio page content updates

--- portfolio.php
+++ portfolio.php
@@ -20,6 +20,41 @@
       <div class="flex-boxes">
 
         <div class="flex-box flex-box-big modal">
+          <img src="img/photogallery/minitab-desktop.png" alt="MiniTab landing page" title="MiniTab landing page" />
+          <i class="fa fa-3x fa-users"></i>
+          <h1 class="flex-title">Frameworks workshop</h1>
+          <p class="field">Speaking</p>
+          <p>Workshop for Carleton students</p>
+          <input class="modal-state" id="modal-1" type="checkbox" />
+          <div class="modal-window">
+            <div class="modal-inner">
+              <label class="modal-close" for="modal-1"></label>
+              <h1>Frameworks workshop</h1>
+              <p class="intro">Roles: Speaker and Organizer</p>
+              <p class="body">The Minitab Landing page was created for Kivuto Solutions, in order to replace the previous non-responsive version and give the page a visual update! I was the only person working on developing the website, and it was completed around Feburary 2014. You can find it <a href="http://onthehub.com/minitab/">here</a></p>
+              <img src="img/photogallery/minitab-desktop.png" alt="MiniTab landing page" title="MiniTab landing page" />
+            </div>
+          </div>
+        </div>
+
+        <div  class="flex-box flex-box-big modal">
+          <i class="fa fa-3x fa-code"></i>
+          <h1 class="flex-title">Student Advantage landing page</h1>
+          <p class="field">Web Development</p>
+          <p>Foundation 5, PHP, SASS, form.js & jQuery</p>
+          <input class="modal-state" id="modal-2" type="checkbox" />
+          <div class="modal-window">
+            <div class="modal-inner">
+              <label class="modal-close" for="modal-2"></label>
+              <h1>Student Advantage landing page</h1>
+              <p class="intro">Roles: Web Developer and QA tester</p>
+              <p class="body">The Student Advantage Landing page was created for Kivuto Solutions, in order to create a new streamlined way of generating leads for the Microsoft Student Advantage program. Similar to the Minitab page, I was the only person working on developing the website, and it was completed around April 2014. You can find it <a href="http://kivuto.com/microsoft-student-advantage-deployment/">here</a></p>
+              <img src="img/photogallery/student-advantage.png" alt="MiniTab landing page" title="MiniTab landing page" />
+            </div>
+          </div>
+        </div>
+
+        <div class="flex-box modal">
           <i class="fa fa-3x fa-code"></i>
           <h1 class="flex-title">MiniTab landing page</h1>
           <p class="field">Web Development</p>
@@ -36,22 +71,6 @@
           </div>
         </div>
 
-        <div  class="flex-box modal">
-          <i class="fa fa-3x fa-code"></i>
-          <h1 class="flex-title">Student Advantage landing page</h1>
-          <p class="field">Web Development</p>
-          <p>Foundation 5, PHP, SASS, form.js & jQuery</p>
-          <input class="modal-state" id="modal-2" type="checkbox" />
-          <div class="modal-window">
-            <div class="modal-inner">
-              <label class="modal-close" for="modal-2"></label>
-              <h1>Student Advantage landing page</h1>
-              <p class="intro">Roles: Web Developer and QA tester</p>
-              <p class="body">The Student Advantage Landing page was created for Kivuto Solutions, in order to create a new streamlined way of generating leads for the Microsoft Student Advantage program. Similar to the Minitab page, I was the only person working on developing the website, and it was completed around April 2014. You can find it <a href="http://kivuto.com/microsoft-student-advantage-deployment/">here</a></p>
-              <img src="img/photogallery/student-advantage.png" alt="MiniTab landing page" title="MiniTab landing page" />
-            </div>
-          </div>
-        </div>
 
         <div class="flex-box modal">
           <i class="fa fa-3x fa-paint-brush"></i>
@@ -86,7 +105,7 @@
         </div>
 
         <div class="flex-box modal">
-          <i class="fa fa-3x fa-code"></i>
+          <i class="fa fa-3x fa-paint-brush"></i>
           <h1 class="flex-title">MyShopify</h1>
           <p class="field">Web Development and Illustration</p>
           <p>Sass, Illustrator & Vectorscribe</p>
@@ -96,7 +115,7 @@
               <label class="modal-close" for="modal-1"></label>
               <h1>MyShopify</h1>
               <p class="intro">Intro text lorem ipsum dolor sit ametm, quas, eaque facilis aliquid cupiditate tempora cumque ipsum accusantium illo modi commodi  minima.</p>
-              <p class="body">Body text lorem ipsum dolor ipsum dolor sit sit possimus amet, consectetur adipisicing elit. Itaque, placeat, explicabo, veniam quos aperiam molestias eriam molestias molestiae suscipit ipsum enim quasi sit possimus quod atque nobis voluptas earum odit accusamus quibusdam.</p>
+              <p class="body">I created these stickmen for my application to Shopify in May 2014, giving illustrations to each slide explaining various topics such as 'The amazing environment at Shopify' and 'The amazing team at Shopify'! If you want to look at the illustrations and website, check them out <a href="http://iryanclarke.com/myshopify/">here</a> </p>
             </div>
           </div>
         </div>
@@ -121,30 +140,7 @@
     </section>       
        
         <div class="section5">  
-            <div class="content minitab-lp hide">
-                <h3>MiniTab landing page</h3>
-                <p>The Minitab Landing page was created for Kivuto Solutions, in order to replace the previous non-responsive version and give the page a visual update! I was the only person working on developing the website, and it was completed around Feburary 2014. You can find it <a href="http://onthehub.com/minitab/">here</a></p>
-                <p>Roles: Web Developer and QA tester.</p>
-                <p>Technologies used: Foundation 5, PHP, SASS, jQuery</p>
-            </div> 
-            <div class="content sa-lp hide">
-                <h3>Student Advantage landing page</h3>
-                <p>The Student Advantage Landing page was created for Kivuto Solutions, in order to create a new streamlined way of generating leads for the Microsoft Student Advantage program. Similar to the Minitab page, I was the only person working on developing the website, and it was completed around April 2014. You can find it <a href="http://kivuto.com/microsoft-student-advantage-deployment/">here</a></p>
-                <p>Roles: Web Developer and QA tester.</p>
-                <p>Technologies used: Foundation 5, PHP, SASS, jQuery, form.js</p>
-            </div> 
-            <div class="content et-poster hide">
-                <h3>ET Movie poster</h3>
-                <p>This was a side project I did just to experiment around with Adobe Illustrator, and creating some complex vector objects.</p>
-                <p>Roles: Designer</p>
-                <p>Technologies used: Adobe Illustrator and VectorScribe</p>
-            </div> 
-            <div class="content scroller hide">
-                <h3>Scroller Guage</h3>
-                <p>This was a little hacking project I worked on, I intended it to put on any scrolling/parallax websites I made to give a cool visual representation of how far you had scrolled. It's still under development, but I intend for the max value to automatically be set to the document height! You can find it at <a href="http://iryanclarke.com/scroller-guage/">here</a></p>
-                <p>Roles: Web Developer and awesome hacker!</p>
-                <p>Technologies used: Ember.js and jQuery</p>
-            </div> 
+ 
             <div class="content illustrated-stick hide">
                 <h3>Illustrated stickmen</h3>
                 <p>I created these stickmen for my application to Shopify in May 2014, giving illustrations to each slide explaining various topics such as 'The amazing environment at Shopify' and 'The amazing team at Shopify'! If you want to look at the illustrations and website, check them out <a href="http://iryanclarke.com/myshopify/">here</a> </p>
@@ -159,45 +155,7 @@
             </div>
         </div>   
     
-        <div class="section1">  
-        	<main>
-            <h3>Click on the images to see more information!</h3>
-                <hr />
-                <div class="photoset">
-                    <div class="photoset-row">
-                        <figure class="photoset-item">
-                            <a onClick="showContent('.minitab-lp')"><img src="img/photogallery/minitab-desktop.png" alt="MiniTab landing page" title="MiniTab landing page" /></a>
-                            <figcaption><h3>MiniTab landing page</h3> <h4>Web Development</h4></figcaption>
-                        </figure>
-                    </div>
-                    <div class="photoset-row">
-                        <figure class="photoset-item">
-                            <a onClick="showContent('.sa-lp')"><img src="img/photogallery/student-advantage.png" alt="MiniTab landing page" title="MiniTab landing page" /></a>
-                            <figcaption><h3>Student Advantage landing page.</h3> <h4>Web Development</h4></figcaption>
-                        </figure>
-                        <figure class="photoset-item">
-                            <a onClick="showContent('.et-poster')"><img src="img/photogallery/et-poster.png" alt="'E.T.' Movie poster" title="'E.T.' Movie poster" /></a>
-                            <figcaption><h3>'E.T.' Movie poster</h3> <h4>Illustration</h4></figcaption>
-                        </figure>
-                    </div>
-                    <div class="photoset-row">
-                        <figure class="photoset-item">
-                            <a onClick="showContent('.scroller')"><img src="img/photogallery/scrollerguage.png" alt="Web Application" title="Web Application" /></a>
-                            <figcaption><h3>Dynamic scroller guage</h3> <h4>Web Application</h4></figcaption>
-                        </figure>
-                        <figure class="photoset-item">
-                            <a onClick="showContent('.illustrated-stick')"><img src="img/photogallery/stickmen-illustrations.png" alt="Stickmen for Shopify application" title="Stickmen for Shopify application" /></a>
-                            <figcaption><h3>Awesome stickmen</h3> <h4>Illustration</h4></figcaption>
-                        </figure>
-                        <figure class="photoset-item">
-                            <a onClick="showContent('.falling-motion')"><img src="img/photogallery/fallingmotion.png" alt="Falling Motion parallax website" title="Falling Motion parallax website" /></a>
-                            <figcaption><h3>Falling Motion parallax website</h3> <h4>Web Development</h4></figcaption>
-                        </figure>
-                    </div>
-                </div>
-            </main>
-        <p>&nbsp;</p>
-        </div>   
+        
         
     <script src="js/gAnalytics.js"></script> 
          
